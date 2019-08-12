9fa2b7ed322930e3dc7474af33dd18e432f70688
statup-github <35571976+statup-github@users.noreply.github.com>
2019-08-12 11:53:04 +0200

    Add separate healthcode (part 1)

--- apache.sh
+++ apache.sh
@@ -2,22 +2,34 @@
 
 set -e
 
-# Create passphrase
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 > /usr/local/apache2/conf/cryptopassphrase
+# Insert cryptopassphrase. Create passphrase if none is set
+touch /usr/local/apache2/conf/cryptopassphrase
 chmod 0400 /usr/local/apache2/conf/cryptopassphrase
+cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 > /usr/local/apache2/conf/cryptopassphrase
 
-# insert into config
 sed -ri "s/replacemewithpassphrase/$(cat /usr/local/apache2/conf/cryptopassphrase)/g" /usr/local/apache2/conf/httpd.conf
 
-# Insert the code that is used for authentication the Selenium test IDE
+# Insert the code that is used for healthchecks
 # Fallback code if no value is set for the container
-cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 > /usr/local/apache2/conf/testcode
+touch /usr/local/apache2/conf/healthcode
+chmod 0400 /usr/local/apache2/conf/healthcode
+cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 > /usr/local/apache2/conf/healthcode
+
+if [ ${#healthcode} -lt 16 ]; then healthcode=$(cat /usr/local/apache2/conf/healthcode); fi
+if [ ${#healthcode} -lt 16 ]; then echo "healthcode not set or too short"; exit; fi
+
+sed -ri "s/replacemewithhealthcode/${healthcode}/g" /usr/local/apache2/conf/httpd.conf
+
+
+# Insert the code that is used for authenticating automated Selenium tests
+# Fallback code if no value is set for the container
+touch /usr/local/apache2/conf/testcode
 chmod 0400 /usr/local/apache2/conf/testcode
+cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1 > /usr/local/apache2/conf/testcode
 
 if [ ${#testcode} -lt 16 ]; then testcode=$(cat /usr/local/apache2/conf/testcode); fi
 if [ ${#testcode} -lt 16 ]; then echo "testcode not set or too short"; exit; fi
 
-# Insert the code that is used for authentication the Selenium test IDE
 sed -ri "s/replacemewithtestcode/${testcode}/g" /usr/local/apache2/conf/httpd.conf
 
 httpd-foreground
